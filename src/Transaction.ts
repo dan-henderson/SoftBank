9ca6b16ddcfc78ab065cbbd8ce5b47da582f7cc4
Dan Henderson <daniel.henderson@softwire.com>
2016-07-07 12:13:30 +0100

    Added Transaction class, added addTransaction function into Account class.

---
+++ src/Transaction.ts
@@ -0,0 +1,20 @@
+
+class Transaction {
+    date: string;
+    from: string;
+    to: string;
+    narrative: string;
+    amount: number;
+    constructor(
+        date:string,
+        from: string,
+        to: string,
+        narrative: string,
+        amount: number) {
+            this.date = date;
+            this.from = from;
+            this.to = to;
+            this.narrative = narrative;
+            this.amount = amount;
+        }
+}
--- src/account.ts
+++ src/account.ts
@@ -2,7 +2,18 @@
 class Account {
     holder: string;
     balance: number;
+    transactions: Transaction[];
     constructor(name: string) {
         this.holder = name;
+        this.balance = 0;
+    }
+
+    addTransaction(transaction: Transaction) {
+        if (transaction.to == this.holder) {
+            this.balance += transaction.amount;
+        } else {
+            this.balance -= this.balance + transaction.amount;
+        }
+        this.transactions.push(transaction);
     }
 }
