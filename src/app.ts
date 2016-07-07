202207c0828c5db81114e05d826c954b69faede0
Dan Henderson <daniel.henderson@softwire.com>
2016-07-07 16:26:46 +0100

    Added code to read in data from 'Transactions2014.csv', and convert this in to an array of Transactions.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,59 @@
+import fs = require('fs');
+import * as readline from 'readline';
+import {Transaction} from './Transaction';
+import {Account} from './Account';
+
+let data = fs.readFileSync('Transactions2014.csv', 'utf8');
+//console.log(data);
+
+let transactions = parseCSV(data);
+console.log(transactions);
+
+let accounts: Account[] = workOutAccounts(transactions);
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+rl.question('Input command: ', handleCommand);
+
+function handleCommand(input: string) {
+    if (input == "exit") {
+        rl.close();
+        process.exit();
+    } else {
+        // code to handle command
+        rl.question('Input command: ', handleCommand);
+    }
+};
+
+function parseCSV(data: string) : Transaction[] {
+    let split1: string[] = data.split('\r\n');
+    let data_array: string[] = [];
+    for (var i = 0; i < split1.length; i++) {
+        let split2: string[] = split1[i].split(',');
+        for (var j = 0; j < split2.length; j++) {
+            data_array.push(split2[j]);
+        }
+    }
+    data_array.pop();
+    //console.log(data_array);
+
+    let transactions: Transaction[] = [];
+    for (var i = 0; i < data_array.length/5; i++) {
+        transactions.push(new Transaction(
+            data_array[5*i],
+            data_array[5*i+1],
+            data_array[5*i+2],
+            data_array[5*i+3],
+            parseFloat(data_array[5*i+4])
+            ))
+    }
+    return transactions;
+}
+
+function workOutAccounts(transactions: Transaction[]) : Account[] {
+    return[];
+}
+
+/*
 import {Account} from './Account';
 import {Transaction} from './Transaction';
 
@@ -14,3 +70,4 @@ first_account.addTransaction(first_transaction);
 console.log(first_account);
 
 process.exit();
+*/
